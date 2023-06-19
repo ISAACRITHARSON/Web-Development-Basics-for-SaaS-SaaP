1e682fce673b671a3717c280a66e4df62b27d9fc
ISAAC RITHARSON P <isaacritharson@karunya.edu.in>
2023-06-19 23:59:19 +0530

    Create B.js

---
+++ Working_with_Angular_JS/B.js
@@ -0,0 +1,16 @@
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+$scope.name = [
+"isaac",
+"ashwin",
+  "sri ram",
+"michael ashick",
+"Dhavasakthi"
+];
+$scope.subject = [
+"Database management systems",
+"Web Technology",
+“Computer Vision",
+"Introduction To Augmented Reality",
+"Geometric Modelling"
+];
+})
